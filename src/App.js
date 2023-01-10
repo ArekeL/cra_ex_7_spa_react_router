5801e713560f5185871eeae41656606e91487a61
Arek Lechnik <alechnik@gmail.com>
2023-01-10 19:06:43 +0100

    React Router -  NavLink, Routes, Route, error page + some css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React, { Component } from "react";
+import {
+	BrowserRouter as Router,
+	NavLink,
+	Routes,
+	Route,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
+import News from "./News";
+import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
 
 class App extends Component {
 	state = {};
 	render() {
-		return <div className='App'>Hallow world</div>;
+		return (
+			<Router>
+				<div className='App'>
+					<header>
+						<nav>
+							<ul>
+								<li>
+									<NavLink to='/'>Home</NavLink>
+								</li>
+								<li>
+									<NavLink to='/news'>News</NavLink>
+								</li>
+								<li>
+									<NavLink to='/about-us'>About Us</NavLink>
+								</li>
+								<li>
+									<NavLink to='/contact'>Contact</NavLink>
+								</li>
+							</ul>
+						</nav>
+					</header>
+					<section>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/news' element={<News />} />
+							<Route path='/about-us' element={<AboutUs />} />
+							<Route path='/contact' element={<Contact />} />
+							<Route path='*' element={<ErrorPage />} />
+						</Routes>
+					</section>
+				</div>
+			</Router>
+		);
 	}
 }
 
---
+++ src/ErrorPage.js
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+	return (
+		<>
+			<h1>404</h1>
+			<p>Sorry something went wrong </p>
+		</>
+	);
+};
+
+export default ErrorPage;
